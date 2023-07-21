2ec7b088f3b19a834535ad2326f79221698774d9
Travis <travisformayor@users.noreply.github.com>
2023-07-20 22:20:31 -0600

    fix color bug

--- Kanban.cpp
+++ Kanban.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+const WORD TEXT_WHITE = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+const WORD TEXT_GREEN = FOREGROUND_INTENSITY | FOREGROUND_GREEN;
+
 map<string, string> screenMenus = {
     {"Boards", "| up/down: Navigate | enter: Select | n: New Board | d: Delete Board | b: Back | esc: Quit |"},
     {"Board", "| up/down: Navigate | enter: Select Task | d: Delete Task | b: Back | esc: Quit | "},
@@ -12,11 +15,12 @@ map<string, string> screenMenus = {
 };
 
 void topMenu(const string& currentScreen) {
-    cout << "================================ Kanban Board ================================\n";
+    cout << "======================================= Kanban Board =======================================\n";
     cout << screenMenus[currentScreen] << "\n";
-    cout << "======================= (Press key to make selection) ========================\n";
+    cout << "============================== (Press key to make selection) ===============================\n";
     cout << "\n";
-    cout << "| " << currentScreen << " |\n";
+    cout << "                                       | " << currentScreen << " |\n";
+    cout << "\n";
 }
 
 // set the console text color
@@ -27,19 +31,20 @@ void setTextColor(WORD color) {
 
 // display list with selection highlighted
 void displayList(const list<string>& items, int selectedIndex) {
-
     int index = 0;
     for (const auto& item : items) {
+        // 1) set the color
         if (index == selectedIndex) {
-            setTextColor(FOREGROUND_INTENSITY | FOREGROUND_BLUE); // Highlighted item color
+            setTextColor(TEXT_GREEN); // highlighted item color
         }
         else {
-            setTextColor(FOREGROUND_INTENSITY | FOREGROUND_GREEN); // Regular item color
+            setTextColor(TEXT_WHITE); // regular item color
         }
-
+        // 2) print the item
         cout << item << endl;
         index++;
     }
+    setTextColor(TEXT_WHITE); // Reset color to white
 }
 
 void displayScreen(const string& currentScreen, const list<string>& items, int selectedIndex) {
@@ -50,6 +55,7 @@ void displayScreen(const string& currentScreen, const list<string>& items, int s
 }
 
 int main() {
+    // test items for ui
     list<string> items = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
     int selectedIndex = 0;
 
