43d47c1a5ea9e504d9bc1dcbf28a37fcd4cc4eb9
Travis <travisformayor@users.noreply.github.com>
2023-07-25 10:40:45 -0600

    remove duplicate deallocate

--- UI.cpp
+++ UI.cpp
@@ -397,44 +397,33 @@ void UI::addNewTask() {
 }
 
 void UI::deleteSelectedBoard() {
-    try {
-        // check if there are boards
-        if (this->loadedBoards.size() > 0) {
-            // to do: remove debug couts
-            cout << "1. Index: " + to_string(selectedIndex) + ". List size: " + to_string(this->loadedBoards.size()) << endl;
-            // find the selected board
-            list<Board*>::iterator boardIter = this->loadedBoards.begin();
-            advance(boardIter, this->selectedIndex);
+    // check if there are boards
+    if (this->loadedBoards.size() > 0) {
+        // to do: remove debug couts
+        cout << "1. Index: " + to_string(selectedIndex) + ". List size: " + to_string(this->loadedBoards.size()) << endl;
+        // find the selected board
+        list<Board*>::iterator boardIter = this->loadedBoards.begin();
+        advance(boardIter, this->selectedIndex);
 
-            Board* boardPtr = *boardIter;
-            cout << "Board ID: " + to_string(boardPtr->getId()) << endl;
-            cout << "Board Title: " + boardPtr->getTitle() << endl;
+        Board* boardPtr = *boardIter;
+        cout << "Board ID: " + to_string(boardPtr->getId()) << endl;
+        cout << "Board Title: " + boardPtr->getTitle() << endl;
 
-            // delete board from DB and deallocated memory
-            this->db.deleteBoard(**boardIter); // deref iterator gets board ptr, then deref ptr
-            cout << "Deleted board" << endl;
-            delete* boardIter; // deref iterator returns board*
-            cout << "Deallocated board pointer" << endl;
-            // reload list of boards
-            reloadBoards();
-            cout << "Reloaded boards" << endl;
-            // fix selected index if was at end of list
-            this->selectedIndex = max(0, static_cast<int>(this->loadedBoards.size()) - 1);
-            cout << "2. Index: " + to_string(selectedIndex) + ". List size: " + to_string(this->loadedBoards.size()) << endl;
-        }
-        else {
-            cout << "No boards to delete." << endl;
-            this->selectedIndex = 0;
-        }
+        // delete board from DB and deallocated memory
+        this->db.deleteBoard(**boardIter); // deref iterator gets board ptr, then deref ptr
+        cout << "Deleted board" << endl;
+        // delete* boardIter; // deref iterator returns board*
+        // cout << "Deallocated board pointer" << endl;
+        // reload list of boards (deallocates all board pointers)
+        reloadBoards();
+        cout << "Reloaded boards" << endl;
+        // fix selected index if was at end of list
+        this->selectedIndex = max(0, static_cast<int>(this->loadedBoards.size()) - 1);
+        cout << "2. Index: " + to_string(selectedIndex) + ". List size: " + to_string(this->loadedBoards.size()) << endl;
     }
-    catch (runtime_error& e) {
-        cerr << "A runtime error occurred: " << e.what() << endl;
-    }
-    catch (exception& e) {
-        cerr << "An exception occurred: " << e.what() << endl;
-    }
-    catch (...) {
-        cerr << "An unknown error occurred." << endl;
+    else {
+        cout << "No boards to delete." << endl;
+        this->selectedIndex = 0;
     }
 }
 
